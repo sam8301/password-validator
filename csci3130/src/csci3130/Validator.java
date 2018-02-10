a2b4d42dba9000d89e3f5db67b08f92d19a0fc0f
Jesse <tianxiaoyu19950627@gmail.com>
2018-02-10 02:34:35 -0400

    second

--- csci3130/src/csci3130/Validator.java
+++ csci3130/src/csci3130/Validator.java
@@ -9,14 +9,27 @@ public class Validator {
 		if(password == null || password.isEmpty()) {
 			return 0;
 		}
-		
+		//not password
 		if(! password.equalsIgnoreCase("password")) {
 			passtime++;
 		}
-			
+		//longer than 8	
 	    if(password.length() >= 8 ) {
 			passtime++;
 		}
+	    //requiring at least 1 special character
+	    if(!password.matches("[a-zA-Z0-9 ]*")) {
+	    		passtime++;
+	    }
+	    //at least 1 digit
+	    if(password.matches(".*\\d.*")) {
+	    	  	passtime++;
+	    }
+	    //both upper and lower case
+	    if(!password.equals(password.toLowerCase()) && !password.equals(password.toUpperCase())) {
+	    		passtime++;
+	    }
+	    
 
 		return passtime;	
 	}
--- csci3130/src/csci3130/ValidatorTest.java
+++ csci3130/src/csci3130/ValidatorTest.java
@@ -9,13 +9,18 @@ class ValidatorTest {
 	@Test 
 
 	public void testCase1() {
-		
-		//test for     it is not “password” (case insensitive)
-		// and         it is at least 8 characters long
+        //2
 		assertEquals(Validator.case1("password"),1);
-		assertEquals(Validator.case1("PassWord"),1);
-		assertEquals(Validator.case1("asdf"),1);
-		assertEquals(Validator.case1("sdjijfiweofijsi"),2);
+		//2,5
+		assertEquals(Validator.case1("PassWord"),2);
+		//1,3
+		assertEquals(Validator.case1("asdf#"),2);
+		//1,2,4
+		assertEquals(Validator.case1("sdjijfieo0"),3);
+		//1,2,4,5
+		assertEquals(Validator.case1("sdjijfieo0#"),4);
+		//1,2,3,4,5
+		assertEquals(Validator.case1("sdDjijfieo0#"),5);
 		
 	}
 
